adfa4fe28e7e42e6581c66ba1452525b5f1b91df
gemelodyyu <yuge.chang0512@gmail.com>
2020-08-06 21:03:51 -0500

    update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,15 +2,61 @@
 import csv
 import os
 
-# create variable for the path
-poll_path = os.path.join("Resources", "election_data.csv")
-
 # create variables
+candidate_list = []
+candidate_votes = []
+total_votes = 0  
+#election_data = ["1", "2"]
 
+# start for loop 
+#for files in election_data: 
+    # create variable for the path
+poll_path = os.path.join("Resources", "election_data.csv")
 
 # read csv data
 with open(poll_path) as csv_file: 
-	csv_reader = csv.reader(csv_file, delimiter=',')
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    #skip the header line
+    csv_header = next(csv_reader)
+    
+    # use for loop to go though the election_data
+    for row in csv_reader: 
 
-	#skip the header line
-	csv_header = next(csv_reader)
+        total_votes = total_votes +1
+        candidate = row[2]
+
+        # calculate running total for each candidate 
+        if candidate in candidate_list:
+            index = candidate_list.index(candidate)
+            candidate_votes[index] = candidate_votes[index] +1 
+            
+        # make new spot for another candidate
+        else:
+            candidate_list.append(candidate)
+            candidate_votes.append(1)
+
+    # create temp variables
+    candidate_percentages = []
+    max_vote = candidate_votes[0]
+    max_index = 0
+
+    # calculate percentage each candidate win and find the winner
+    for i in range(len(candidate_list)): 
+        percentage = round(candidate_votes[i]/total_votes*100, 2) 
+        candidate_percentages.append(percentage)
+        #find winner
+        if candidate_votes[i] > max_vote:
+            max_vote = candidate_votes
+            max_index = i
+    winner = candidate_list[max_index]
+
+    # print results
+    print("Election Results")
+    print("-----------------------------")
+    print(f"Total Votes: {total_votes}")
+    print("-----------------------------")
+    for i in range(len(candidate_list)):
+        print(f"{candidate_list[i]}: {candidate_percentages[i]}% ({candidate_votes[i]})")
+    print("-----------------------------")
+    print(f"Winner:  {winner}")
+    print("-----------------------------")
